5a787f7b17f808df7b9d599c0515f968bca52b06
mcalheiro <mauriciocalheiro95@gmail.com>
2024-02-17 14:02:14 +0000

    Section 2: the basics
    - Create some views and urls
    - Create templates
    - Render data to templates

--- website/projects/views.py
+++ website/projects/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+projects_list = [
+    {
+        'id':'1',
+        'title':'Ecommerce website',
+        'description':'Fully functional ecommerce website'
+    },
+    {
+        'id':'2',
+        'title':'Portfolio website',
+        'description':'This is my portfolio'
+    },
+    {
+        'id':'3',
+        'title':'Social network',
+        'description':'Remember facebook?'
+    },
+]
+
+def projects(request):
+    page = 'projects'
+    number = 10
+    context = {'page': page, 'number': number, 'projects': projects_list}
+    return render(request, 'projects/projects.html', context)
+
+def project(request, pk):
+    projectObj = None
+    for i in projects_list:
+        if i['id'] == str(pk):
+            projectObj = i
+    return render(request, 'projects/single-projects.html', {'project': projectObj})
