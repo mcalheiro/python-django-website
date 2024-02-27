a89e3525761be3d5152cf1f28343f178c2044591
mcalheiro <mauriciocalheiro95@gmail.com>
2024-02-27 12:07:06 +0000

    Create model form

--- website/projects/urls.py
+++ website/projects/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.projects, name='projects'),
     path('project/<pk>', views.project, name='project'),
+    path('create-project/', views.createProject, name='create-project'),
 ]
