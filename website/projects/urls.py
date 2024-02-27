fb00c550269d05308c76113d72347bc09a01ed91
mcalheiro <mauriciocalheiro95@gmail.com>
2024-02-27 13:00:10 +0000

    Section 4: CRUD
    - Models forms and CRUD
    - Implement createProject, updateProject and deleteProject
    - Implement delete template

--- website/projects/urls.py
+++ website/projects/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.projects, name='projects'),
     path('project/<pk>', views.project, name='project'),
     path('create-project/', views.createProject, name='create-project'),
+    path('update-project/<pk>', views.updateProject, name='update-project'),
+    path('delete-project/<pk>', views.deleteProject, name='delete'),
 ]
